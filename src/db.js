73a388fdebeba20bd6b582b141949ce3d09e8d0e
Malachy Egbuna <zubbyik@gmail.com>
2019-12-12 20:57:30 +0100

    update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongo_uri = process.env.MONGOLAB_URI || 'mongodb://localhost/report'
+const mongo_uri = process.env.MONGOLAB_URI || 'mongodb://zubbyik:!1Winner75@ds353748.mlab.com:53748/heroku_f1t129zj'
 
 mongoose.Promise = Promise
 mongoose.connect(mongo_uri, (err, res) => {
